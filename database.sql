72af58d7f726fd92d8414e638801ae2825385a50
SafwenHali <93746781+SafwenHali@users.noreply.github.com>
2021-12-29 07:58:01 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -7,12 +7,13 @@ pnomJoueur VARCHAR(8),
 dateNaisJoueur DATE,
 telJoueur int(8)
 );
+
 CREATE TABLE jeux(
 idJeux VARCHAR(3) PRIMARY KEY,
 nomJeux VARCHAR(8),
 minNbr int,
 maxNbr int
-)
+);
 
 CREATE TABLE tournoi (
 idT VARCHAR(3) PRIMARY KEY,
