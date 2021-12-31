4eed667b8758b9b3b7ad0d384142622b9eb0598c
SafwenHali <93746781+SafwenHali@users.noreply.github.com>
2021-12-31 03:44:23 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,23 +1,41 @@
 CREATE DATABASE gestionTournoi;
 
+CREATE TABLE jeux(
+idJeux VARCHAR(3) PRIMARY KEY,
+nomJeux VARCHAR(8),
+minNbr INT DEFAULT 1,
+maxNbr INT DEFAULT 1
+);
+
+CREATE TABLE tournoi (
+idT VARCHAR(3) PRIMARY KEY,
+titreTournoi VARCHAR(20),
+typeJeux VARCHAR(3),
+CONSTRAINT fk_jeuxT FOREIGN KEY typeJeux REFERENCES jeux(idJeux)
+);
+
+CREATE TABLE match (
+idM VARCHAR(3) PRIMARY KEY,
+titreMatch VARCHAR(50),
+tournoiM VARCHAR(3),
+typeJeux VARCHAR(3),
+CONSTRAINT fk_jeuxM FOREIGN KEY typeJeux REFERENCES jeux(idJeux),
+CONSTRAINT fk_tournoiM FOREIGN KEY tournoiM REFERENCES tournoi(idT)
+);
+
+CREATE TABLE equipe (
+    idEq VARCHAR(3) PRIMARY KEY,
+    nomEquipe VARCHAR(20),
+    typeJeux VARCHAR(3),
+CONSTRAINT fk_jeuxE FOREIGN KEY typeJeux REFERENCES jeux(idJeux),
+);
+
 CREATE TABLE joueur (
 idJoueur VARCHAR(3) PRIMARY KEY,
 nomJoueur VARCHAR(8),
 pnomJoueur VARCHAR(8),
 dateNaisJoueur DATE,
-telJoueur int(8)
-);
-
-CREATE TABLE jeux(
-idJeux VARCHAR(3) PRIMARY KEY,
-nomJeux VARCHAR(8),
-minNbr int,
-maxNbr int
-);
-
-CREATE TABLE tournoi (
-idT VARCHAR(3) PRIMARY KEY,
-nomT VARCHAR(8),
-typeJeux VARCHAR(8),
-
+telJoueur INT(8),
+equipeJ VARCHAR(3),
+CONSTRAINT fk_equipeJ FOREIGN KEY equipeJ REFERENCES equipe(idEq)
 );
